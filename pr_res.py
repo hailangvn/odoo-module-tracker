18ef9b09a44f811eed79fcc0b1de683a57877e32
Hai Lang <hailn@trobz.com>
2025-07-30 10:26:26 +0700

    [IMP] more regex to match PR titles

--- pr_res.py
+++ pr_res.py
@@ -12,37 +12,46 @@ pr_res = [
         r"\[*[0-9.]+\]*\s*\[*(ADD|MIG|mig)\]*\s*Migrate\s+([a-z0-9_]+)"
     ),
     re.compile(
-        r"\[(ADD|MIG|mig)\]\s*Added (a\s+)*([a-z0-9_]+) [Mm]odule"
+        r"\[(ADD|MIG|mig)\]\s*[Aa]dded (a\s+|migrated\s+)*([a-z0-9_]+) [Mm]odule"
     ),
     re.compile(
-        r"\[*(ADD|MIG|mig)\]*\s*:*\smigrated\s([^:.,]+)\s+[Mm]odule"
+        r"\[*(ADD|MIG|mig)\]*\s*:*\smigrated\s([^:.,*]+)\s+[Mm]odule"
     ),
     re.compile(
-        r"\[*(ADD|MIG|mig)\]*\s*([Aa]dd\s+[Mm]odule\s+)+([^:.,]+)"
+        r"\[*ADD\]*\s*(([Aa]dd\s+)*[Mm]odule\s+)+([^:.,*]+)"
     ),
     re.compile(
-        r"\[*(ADD|MIG|mig)\]*\s*([Aa]dd\s+)*([^:.,]+)\s+[Mm]odule"
+        r"\[ADD\]\s*:\s+([Aa]dd\s+)+([^:.,*]+)"
     ),
     re.compile(
-        r"\[*(ADD|MIG|mig)\]*\s*([Aa]dd\s+)+([^:.,]+)"
+        r"\[(ADD|MIG|mig)\]\s*([Aa]dd\s+)*([^:.,*]+)\s+[Mm]odule"
     ),
     re.compile(
-        r"\[*[0-9.]+\]*\s*\[*(ADD|MIG|mig)(\+MIG)*(\+REF)*\]*\s+([^:.,]+)"
+        r"(ADD|MIG|mig)\s+([^:.,*]+)\s+[Mm]odule"
+    ),
+    re.compile(
+        r"\[*(ADD|MIG|mig)\]*\s*([Aa]dd\s+)+([^:.,*]+)"
+    ),
+    re.compile(
+        r"\[*[0-9.]+\]*\s*\[*(ADD|MIG|mig)(\+MIG)*(\+REF)*\]*(\[IMP\])?\s+([^:.,*]+)"
     ),
     re.compile(
         r"\[*[0-9.]+\]*\s*\[*(ADD|MIG|mig)\]*\s*([a-z0-9_]+)"
     ),
     re.compile(
+        r"\Backport of ([^:.,*]+) to "
+    ),
+    re.compile(
         r"\[(ADD|MIG|mig)\]\s*\[([0-9.]*)*\]\s*([a-z0-9_]+)"
     ),
     re.compile(
-        r"\[(ADD|MIG|mig)\]\s*\[*([0-9.]+)\]*\s*:*\s*([^:.,]+)"
+        r"\[(ADD|MIG|mig)\]\s*\[*([0-9.]+)\]*\s*:*\s*([^:.,*]+)"
     ),
     re.compile(
-        r"\[(ADD|MIG|mig)\]\[([^:.,\]]+)\]"
+        r"\[(ADD|MIG|mig)\]\[([^:.,*\]]+)\]"
     ),
     re.compile(
-        r"\[(ADD|MIG|mig)\]\s+([^:.,]+)"
+        r"\[(ADD|MIG|mig)\]\s+([^:.,*]+)"
     ),
     re.compile(
         r"\[*([0-9.]*)*\]*\s*\[*(ADD|MIG|mig)\]*(\s*\[[^]]*\])*\s*"
